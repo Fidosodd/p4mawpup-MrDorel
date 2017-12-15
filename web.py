6b84340962d570960a72dba88405493611fba352
Morgan Thomas <33166036+Fidosodd@users.noreply.github.com>
2017-12-15 14:28:06 -0800

    It can detect changes between running.
    It also formats output differently.

--- web.py
+++ web.py
@@ -8,37 +8,33 @@ def main ():
 	
 	sec = 500
 	timeT = 0
+    htmlfile = urllib.urlopen("http://www.nasdaq.com/symbol/aapl/real-time")
+	htmltext = htmlfile.read()
+	regex = '<div id="qwidget_lastsale" class="qwidget-dollar">(.+?)</div>'	
+	pattern = re.compile(regex)
+	oldPrice = re.findall(pattern,htmltext)
 
 	while timeT < sec:
 	
 		print "Checking the current price of apple stock. This may take a few a few minutes..."
-		time.sleep(5)
-		htmlfile = urllib.urlopen("http://www.nasdaq.com/symbol/aapl/real-time")
-		htmltext = htmlfile.read()
-		regex = '<div id="qwidget_lastsale" class="qwidget-dollar">(.+?)</div>'	
-		pattern = re.compile(regex)
-		oldPrice = re.findall(pattern,htmltext)
-		time.sleep(60)
+		time.sleep(65)
 		htmlfile = urllib.urlopen("http://www.nasdaq.com/symbol/aapl/real-time")
 		htmltext = htmlfile.read()
 		regex = '<div id="qwidget_lastsale" class="qwidget-dollar">(.+?)</div>'	
 		pattern = re.compile(regex)
 		newPrice = re.findall(pattern,htmltext)
 		time.sleep(5)
-		print " The current price of apple stock is: "
-		print newPrice
+		print " The old price of apple stock is: "+str(oldPrice)
 		
 		if newPrice != oldPrice:
 			print "---------------------Price changed!---------------------"
-			print "The new Apple stock price is:"
+			print " The new Apple stock price is: "+str(newPrice)
 			oldPrice = newPrice
 		
 		else:
-			print "The new Apple stock price is:"
+			print " The Apple stock price has not changed."
 			timeT +=1
 			
-		print newPrice
-		
 		if timeT == sec:
 			print "Ending process..."
 			break
@@ -49,7 +45,4 @@ def main ():
 	
 
 main()
-
-
-
 
